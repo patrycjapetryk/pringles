25e6b398af3e801b8a952f66d1d2da7b66496c4c
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2023-12-27 19:00:29 +0100

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'pringles-red': '#EA002A',
+        'pringles-dark-red': '#CF102D',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
